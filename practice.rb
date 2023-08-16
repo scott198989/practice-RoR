824c43fd3810f3e10be449578cb85607e597c0a8
Scott Tuschl <scotttuschl@1s-iMac.local>
2023-08-16 13:54:11 -0500

    first commit, a few super basic lines of code

---
+++ practice.rb
@@ -0,0 +1,51 @@
+address = [1,2,3,4,5,6,7,8,9]
+
+p new_address = address.reverse!
+
+p address[2]
+
+p address 
+
+say = "hello"
+
+puts say
+
+hello = "Hello World"
+
+puts hello
+
+def say_hello
+    puts "Hello World!!"
+end
+say_hello()
+
+# how to set up a method
+
+def name(arguement)
+    puts content of method
+end
+
+# remember, every def, do has an end!
+
+first_name = "Scott"
+last_name = "Tuschl"
+
+puts first_name + last_name
+
+# String interprelation must have a pound sign # and curly braces {}
+puts "My first name is #{first_name} and my last name is #{last_name}" 
+
+puts "Enter your first name"
+gets.chomp 
+
+puts "Enter your last name"
+gets.chomp 
+
+first_name = "scott"
+
+last_name = "tuschl"
+
+full_name = first_name + last_name
+
+puts "Your full name is #{full_name}, and your full name reversed is #{full_name.reverse}, and it has #{full_name.length} characters"
+
